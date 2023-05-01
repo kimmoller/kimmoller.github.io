93c32547f271f61806372b939502ca839d92b616
kimmoller <kim.moller2@protonmail.com>
2023-05-01 12:14:44 +0200

    Format code

--- iam-project/mq-schema/src/main/java/com/kimmoller/iamproject/mqschema/dto/AccountLifecycleEventDto.java
+++ iam-project/mq-schema/src/main/java/com/kimmoller/iamproject/mqschema/dto/AccountLifecycleEventDto.java
@@ -1,11 +1,10 @@
 package com.kimmoller.iamproject.mqschema.dto;
 
-import jakarta.validation.constraints.NotNull;
 import com.kimmoller.iamproject.apischema.dto.account.AccountDto;
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
 import lombok.Builder;
 import lombok.Data;
-
-import java.util.UUID;
 
 @Data
 @Builder

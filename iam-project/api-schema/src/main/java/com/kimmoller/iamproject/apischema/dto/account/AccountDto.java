1647c21e9e8a92a53ea05224d2ee5fac4638a2fb
kimmoller <kim.moller2@protonmail.com>
2023-05-01 14:48:14 +0200

    Add constructors to handle object relialization

--- iam-project/api-schema/src/main/java/com/kimmoller/iamproject/apischema/dto/account/AccountDto.java
+++ iam-project/api-schema/src/main/java/com/kimmoller/iamproject/apischema/dto/account/AccountDto.java
@@ -2,11 +2,16 @@ package com.kimmoller.iamproject.apischema.dto.account;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountDto {
   private UUID id;
   private String username;

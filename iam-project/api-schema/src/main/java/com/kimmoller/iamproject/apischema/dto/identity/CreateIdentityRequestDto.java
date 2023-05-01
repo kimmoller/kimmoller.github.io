e7ca8a6d1c53fac1c0dd77cad285e55bb24e3162
Kim Möller <89157030+kimmoller@users.noreply.github.com>
2023-05-01 14:52:27 +0200

    Add constructors to handle object relialization (#10)

--- iam-project/api-schema/src/main/java/com/kimmoller/iamproject/apischema/dto/identity/CreateIdentityRequestDto.java
+++ iam-project/api-schema/src/main/java/com/kimmoller/iamproject/apischema/dto/identity/CreateIdentityRequestDto.java
@@ -2,11 +2,15 @@ package com.kimmoller.iamproject.apischema.dto.identity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Schema(name = "CreateIdentityRequestDto")
 public class CreateIdentityRequestDto {
   @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
--- iam-project/api-schema/src/main/java/com/kimmoller/iamproject/apischema/dto/identity/IdentityDto.java
+++ iam-project/api-schema/src/main/java/com/kimmoller/iamproject/apischema/dto/identity/IdentityDto.java
@@ -4,11 +4,16 @@ import com.kimmoller.iamproject.apischema.dto.account.AccountDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import java.util.UUID;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class IdentityDto {
   private UUID id;
   private String firstName;

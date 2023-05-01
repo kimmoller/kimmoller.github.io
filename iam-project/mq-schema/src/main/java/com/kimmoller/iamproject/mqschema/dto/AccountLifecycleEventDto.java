df0aff182f78b8dbd62e5e50fb8003ba8581c6c0
Kim Möller <89157030+kimmoller@users.noreply.github.com>
2023-05-01 14:52:10 +0200

    Add missing constructors to handle object serialization (#11)

--- iam-project/mq-schema/src/main/java/com/kimmoller/iamproject/mqschema/dto/AccountLifecycleEventDto.java
+++ iam-project/mq-schema/src/main/java/com/kimmoller/iamproject/mqschema/dto/AccountLifecycleEventDto.java
@@ -3,11 +3,16 @@ package com.kimmoller.iamproject.mqschema.dto;
 import com.kimmoller.iamproject.apischema.dto.account.AccountDto;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountLifecycleEventDto {
   @NotNull private AccountDto accountDto;
   @NotNull private UUID identityId;
